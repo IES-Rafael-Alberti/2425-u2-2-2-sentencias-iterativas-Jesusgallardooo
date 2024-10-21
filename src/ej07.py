26b9a2d8bbadb1d8d9c952661e15eeabfa7affdd
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2024-10-21 10:03:14 +0200

    cambio ej07

--- src/ej07.py
+++ src/ej07.py
@@ -1,5 +1,6 @@
 #Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.ç
 
+
 def multiplicar(numero, multiplicador):
     resultado = numero * multiplicador
     return resultado
